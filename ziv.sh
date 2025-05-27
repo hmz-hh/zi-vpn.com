d4919bafde2f06ecfd51284c45145fc3b11810f2
zivpn.com <hamzabenzidane01@gmail.com>
2025-05-27 21:38:24 +0100

    Update ziv.sh
    Signed-off-by: zivpn.com <hamzabenzidane01@gmail.com>

--- ziv.sh
+++ ziv.sh
@@ -3,18 +3,22 @@
 # Creator hamza
 
 echo -e "Updating server"
-sudo apt-get update && apt-get upgrade -y
+sudo apt-get update -qq > /dev/null && sudo apt-get upgrade -y -qq > /dev/null
+
 systemctl stop zivpn.service 1> /dev/null 2> /dev/null
 echo -e "Downloading UDP Service"
-wget https://github.com/hq-mp/zi-vpn.com/raw/refs/heads/main/udp-zivpn-linux-amd64 -O /usr/local/bin/zivpn 1> /dev/null 2> /dev/null
+
+wget -q https://github.com/hq-mp/zi-vpn.com/raw/refs/heads/main/udp-zivpn-linux-amd64 -O /usr/local/bin/zivpn
 chmod +x /usr/local/bin/zivpn
-mkdir /etc/zivpn 1> /dev/null 2> /dev/null
-wget https://raw.githubusercontent.com/hq-mp/zi-vpn.com/refs/heads/main/config.json -O /etc/zivpn/config.json 1> /dev/null 2> /dev/null
+mkdir -p /etc/zivpn 1> /dev/null 2> /dev/null
+wget -q https://raw.githubusercontent.com/hq-mp/zi-vpn.com/refs/heads/main/config.json -O /etc/zivpn/config.json
 
 echo "Generating cert files:"
 openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=California/L=Los Angeles/O=Example Corp/OU=IT Department/CN=zivpn" -keyout "/etc/zivpn/zivpn.key" -out "/etc/zivpn/zivpn.crt"
-sysctl -w net.core.rmem_max=16777216 1> /dev/null 2> /dev/null
-sysctl -w net.core.wmem_max=16777216 1> /dev/null 2> /dev/null
+
+sysctl -w net.core.rmem_max=16777216 > /dev/null 2>&1
+sysctl -w net.core.wmem_max=16777216 > /dev/null 2>&1
+
 cat <<EOF > /etc/systemd/system/zivpn.service
 [Unit]
 Description=zivpn VPN Server
@@ -49,13 +53,15 @@ else
 fi
 
 new_config_str="\"config\": [$(printf "\"%s\"," "${config[@]}" | sed 's/,$//')]"
-
-sed -i -E "s/\"config\": ?\[[[:space:]]*\"zi\"[[:space:]]*\]/${new_config_str}/g" /etc/zivpn/config.json
+sed -i -E "s/\"config\": ?[[:space:]]*\"zi\"[[:space:]]*/${new_config_str}/g" /etc/zivpn/config.json
 
 systemctl enable zivpn.service
 systemctl start zivpn.service
+
 iptables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport 6000:19999 -j DNAT --to-destination :5667
 ufw allow 6000:19999/udp
 ufw allow 5667/udp
+
 rm zi2.* 1> /dev/null 2> /dev/null
+
 echo -e "ZIVPN Installed"
