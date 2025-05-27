11b9839df5f7c6a91496670f567cbbbfef99de8c
zivpn.com <hamzabenzidane01@gmail.com>
2025-05-27 22:27:58 +0100

    Update ziv.sh
    Signed-off-by: zivpn.com <hamzabenzidane01@gmail.com>

--- ziv.sh
+++ ziv.sh
@@ -4,12 +4,11 @@
 # Bash by PowerMX
 
 echo -e "Updating server"
-sudo apt-get update && apt-get upgrade -y
 systemctl stop zivpn.service 1> /dev/null 2> /dev/null
 echo -e "Downloading UDP Service"
 wget https://github.com/zahidbd2/udp-zivpn/releases/download/udp-zivpn_1.4.9/udp-zivpn-linux-amd64 -O /usr/local/bin/zivpn 1> /dev/null 2> /dev/null
 chmod +x /usr/local/bin/zivpn
-mkdir -p /etc/zivpn 1> /dev/null 2> /dev/null
+mkdir /etc/zivpn 1> /dev/null 2> /dev/null
 wget https://raw.githubusercontent.com/zahidbd2/udp-zivpn/main/config.json -O /etc/zivpn/config.json 1> /dev/null 2> /dev/null
 
 echo "Generating cert files:"
@@ -41,28 +40,17 @@ echo -e "ZIVPN UDP Passwords"
 read -p "Enter passwords separated by commas, example: passwd1,passwd2 (Press enter for Default 'zi'): " input_config
 
 if [ -n "$input_config" ]; then
-    IFS=',' read -r -a new_config <<< "$input_config"
-else
-    new_config=("zi")
-fi
-
-# استخراج كلمات السر القديمة من config.json
-old_config=()
-if grep -q '"config":' /etc/zivpn/config.json; then
-    old_config=($(grep '"config":' /etc/zivpn/config.json | sed -E 's/.*(.*).*/\1/' | tr -d '"' | tr ',' ' '))
-fi
-
-# دمج القديم مع الجديد بدون تكرار
-merged_config=("${old_config[@]}")
-for pass in "${new_config[@]}"; do
-    if [[ ! " ${merged_config[*]} " =~ " ${pass} " ]]; then
-        merged_config+=("$pass")
+    IFS=',' read -r -a config <<< "$input_config"
+    if [ ${#config[@]} -eq 1 ]; then
+        config+=(${config[0]})
     fi
-done
+else
+    config=("zi")
+fi
 
-# إعادة كتابة السطر في config.json
-new_config_str="\"config\": [$(printf "\"%s\"," "${merged_config[@]}" | sed 's/,$//')]"
-sed -i -E "s/\"config\": ?[^]]*/${new_config_str}/g" /etc/zivpn/config.json
+new_config_str="\"config\": [$(printf "\"%s\"," "${config[@]}" | sed 's/,$//')]"
+
+sed -i -E "s/\"config\": ?\[[[:space:]]*\"zi\"[[:space:]]*\]/${new_config_str}/g" /etc/zivpn/config.json
 
 systemctl enable zivpn.service
 systemctl start zivpn.service
