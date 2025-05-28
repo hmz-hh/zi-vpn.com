373b4bf7951737551699aff670246be65e912d6d
zivpn.com <hamzabenzidane01@gmail.com>
2025-05-28 17:04:28 +0100

    Update ziv.sh
    Signed-off-by: zivpn.com <hamzabenzidane01@gmail.com>

--- ziv.sh
+++ ziv.sh
@@ -3,14 +3,15 @@
 # Creator hamza
 # Bash by PowerMX
 
-echo -e " Create new accounts"
+echo -e "Updating server"
 systemctl stop zivpn.service 1> /dev/null 2> /dev/null
+echo -e "Downloading UDP Service"
 wget https://github.com/zahidbd2/udp-zivpn/releases/download/udp-zivpn_1.4.9/udp-zivpn-linux-amd64 -O /usr/local/bin/zivpn 1> /dev/null 2> /dev/null
 chmod +x /usr/local/bin/zivpn
 mkdir /etc/zivpn 1> /dev/null 2> /dev/null
 wget https://raw.githubusercontent.com/zahidbd2/udp-zivpn/main/config.json -O /etc/zivpn/config.json 1> /dev/null 2> /dev/null
 
-echo " Generating cert files:"
+echo "Generating cert files:"
 openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=California/L=Los Angeles/O=Example Corp/OU=IT Department/CN=zivpn" -keyout "/etc/zivpn/zivpn.key" -out "/etc/zivpn/zivpn.crt"
 sysctl -w net.core.rmem_max=16777216 1> /dev/null 2> /dev/null
 sysctl -w net.core.wmem_max=16777216 1> /dev/null 2> /dev/null
@@ -35,8 +36,8 @@ NoNewPrivileges=true
 WantedBy=multi-user.target
 EOF
 
-echo -e " ZIVPN UDP Passwords"
-read -p " Enter passwords separated by commas, example: passwd1,passwd2 (Press enter for Default 'zi'): " input_config
+echo -e "ZIVPN UDP Passwords"
+read -p "Enter passwords separated by commas, example: passwd1,passwd2 (Press enter for Default 'zi'): " input_config
 
 if [ -n "$input_config" ]; then
     IFS=',' read -r -a config <<< "$input_config"
@@ -57,4 +58,4 @@ iptables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev
 ufw allow 6000:19999/udp
 ufw allow 5667/udp
 rm zi2.* 1> /dev/null 2> /dev/null
-echo -e " Accounts created successfully"
+echo -e "ZIVPN Installed"
