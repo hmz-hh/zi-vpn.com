d4e1649fc1cc03627309515b8ee60b6f33a77024
hamza <hamzabenzidane01@gmail.com>
2025-07-08 01:08:19 +0100

    Update ziv.sh
    Signed-off-by: hamza <hamzabenzidane01@gmail.com>

--- ziv.sh
+++ ziv.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 # Zivpn UDP Module installer - AMD x64
-# Creator Zahid Islam
-# Bash by PowerMX
+# Creator hmz
+FLAG_FILE="/var/log/.zivpn_update_done"
+if [[ ! -f "$FLAG_FILE" ]]; then
+  DEBIAN_FRONTEND=noninteractive apt-get update -qq > /dev/null 2>&1
+  DEBIAN_FRONTEND=noninteractive apt-get upgrade -y -qq > /dev/null 2>&1
+  touch "$FLAG_FILE"
+fi
 
 systemctl stop zivpn.service 1> /dev/null 2> /dev/null
-echo -e "Downloading UDP Service"
 wget https://github.com/zahidbd2/udp-zivpn/releases/download/udp-zivpn_1.4.9/udp-zivpn-linux-amd64 -O /usr/local/bin/zivpn 1> /dev/null 2> /dev/null
 chmod +x /usr/local/bin/zivpn
 mkdir /etc/zivpn 1> /dev/null 2> /dev/null
 wget https://raw.githubusercontent.com/zahidbd2/udp-zivpn/main/config.json -O /etc/zivpn/config.json 1> /dev/null 2> /dev/null
 
-echo "Generating cert files:"
 openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=California/L=Los Angeles/O=Example Corp/OU=IT Department/CN=zivpn" -keyout "/etc/zivpn/zivpn.key" -out "/etc/zivpn/zivpn.crt"
 sysctl -w net.core.rmem_max=16777216 1> /dev/null 2> /dev/null
 sysctl -w net.core.wmem_max=16777216 1> /dev/null 2> /dev/null
